6e78312ce00b9220fcfb08a53be36a705268a864
Kirill11222211 <akimovkirill24@mail.ru>
2021-12-17 14:28:42 +0300

    Initial commit

---
+++ src/main/java/pr9/Moveable.java
@@ -0,0 +1,9 @@
+package pr9;
+
+public interface Moveable {
+    void moveUp();
+    void moveDown();
+    void moveRight();
+    void moveLeft();
+
+}
---
+++ src/main/java/pr9/Nameable.java
@@ -0,0 +1,5 @@
+package pr9;
+
+public interface Nameable {
+    void getName();
+}
